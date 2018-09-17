1d1e98035350c25dfa415e027b4f4d272498683c
Pascal Mies <pascalniklasmies@gmail.com>
2018-09-17 11:10:53 +0200

    pundit not needed, switch to change layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :about, :resume, :contact]
+
+  def about
+  end
+
+  def resume
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get "/blog", to: "posts#index"
+    get "/about", to: "pages#about"
+    get "/resume", to: "pages#resume"
+    get "/contact", to: "pages#contact"
 
   resources :posts, only: [:show, :new, :create, :destroy]
 end
